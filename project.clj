a3962f1ba007c7a7f54bc22bfcb0aa67ce291599
Joe Wass <jwass@crossref.org>
2017-07-03 12:20:06 +0100

    Remove resources from Dockerfile.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject event-data-hypothesis-agent "0.2.1"
+(defproject event-data-hypothesis-agent "0.2.2"
   :description "Crossref Event Data hypothesis.com Agent"
   :url "http://eventdata.crossref.org"
   :license {:name "The MIT License (MIT)"
